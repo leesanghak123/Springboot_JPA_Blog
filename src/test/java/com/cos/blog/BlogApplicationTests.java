1cbf050811817305da18e0c682080af5cf947041
leesanghak123 <145965779+leesanghak123@users.noreply.github.com>
2024-06-17 10:51:45 +0900

    Update BlogApplicationTests.java

--- src/test/java/com/cos/blog/BlogApplicationTests.java
+++ src/test/java/com/cos/blog/BlogApplicationTests.java
@@ -1,13 +1,13 @@
-package com.cos.blog;
+// package com.cos.blog;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BlogApplicationTests {
+// @SpringBootTest
+// class BlogApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
